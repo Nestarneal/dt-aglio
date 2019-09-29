612dfb6aa7843b51324347c08199e53a204ca50e
Nestarneal <nestarneal@gmail.com>
2019-09-29 22:53:56 +0800

    Add main logic

---
+++ index.js
@@ -0,0 +1,16 @@
+const aglio = require('aglio')
+
+if (process.argv.length !== 4) {
+  console.log('dt-aglio <inputFile> <outputFile>')
+  return
+}
+
+const inputFilePath = process.argv[2]
+const outputFilePath = process.argv[3]
+
+const options = {
+  themeVariables: 'cyborg',
+  themeTemplate: 'triple',
+}
+
+aglio.renderFile(inputFilePath, outputFilePath, options, error => error && console.log(error))
